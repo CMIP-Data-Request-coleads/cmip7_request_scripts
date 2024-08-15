eb2e740dc3ca0f09163caf62bc67548399f8ca51
Martin Juckes <mjuckes@rslmj01.badc.rl.ac.uk>
2024-08-15 09:34:45 +0100

    Writing a test record to sandpit

--- scripts/request_write_test.py
+++ scripts/request_write_test.py
@@ -1,8 +1,9 @@
-
 import request_read as rr
-
+from datetime import date
+today = date.today().ctime()
 
 ## note that read fails if schema read is not enabled.
+## 15th Aug: successfully updates a record. Read of all tables is wasted resource and should be eliminated.
 
 key = open( '/home/mjuckes/Repositories/sandpit_variable_views_write' ).readlines()[1].strip()
 
@@ -11,5 +12,9 @@ lb = rr.LoadBase(api)
 
 x = api.bases()[0]
 lb.load(x)
+
+
+t = lb.table_objs['CF standard name import']
+t.create( {'Name':today, 'Notes':'Test' } )
 
 
